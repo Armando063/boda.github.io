17fec44076ddc2ea1cec6ffa21576e21e3bfbd8b
armando <armando.bonilla063@gmail.com>
2025-08-22 17:37:48 -0600

    prueba final

---
+++ js/invitados.js
@@ -0,0 +1,5 @@
+const invitados = {
+    "0366": { nombre: "Familia Bonilla Hernandez", lugares: 2 },
+    "4380": { nombre: "Familia Prueba", lugares: 2 },
+    "54321": { nombre: "", lugares: 1 }
+  };
